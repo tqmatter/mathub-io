cfc4b8fb65bb8dc452428365eafd0d090f7c3282
Roberto Díaz Pérez <r.r.1994a@gmail.com>
2019-01-14 16:53:25 +0100

    Updates

---
+++ component---src-pages-contact-js-c6ade6f7d672ea6209c8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{141:function(a,e,t){"use strict";t.r(e);var c=t(8),n=(t(0),t(148));e.default=function(){return Object(c.a)(n.a,null,Object(c.a)("div",{className:"contained"},Object(c.a)("h2",null,"Contact"),Object(c.a)("p",null,"Please, feel free to contact us at the email below:"),Object(c.a)("a",{href:"mailto:omdb@diracmaterials.org"},"omdb@diracmaterials.org")))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-c6ade6f7d672ea6209c8.js.map
