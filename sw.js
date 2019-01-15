ebe1a0f67eb430f924fd9c2485282ea923100f30
Roberto Díaz Pérez <r.r.1994a@gmail.com>
2019-01-15 09:39:42 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-450eae25074eadee5ef6.js"
+    "url": "webpack-runtime-ed915b1fa561fc6bd9a4.js"
   },
   {
     "url": "app-3c2c25afb706acc7c425.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f5635a5209864d998afac04856a91f11"
+    "revision": "8a47fab396ee1920f5cdeda9c0bf8d45"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
